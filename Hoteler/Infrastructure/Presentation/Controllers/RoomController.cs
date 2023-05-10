55515624aeceb350535e3984464862530d6c19d2
Petr Kruchinin <kruchinin.pea@mmk.ru>
2023-05-10 09:23:16 +0500

    add infrastructure layers

---
+++ Hoteler/Infrastructure/Presentation/Controllers/RoomController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/room")]
+    public class RoomController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public RoomController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager ?? throw new ArgumentNullException(nameof(serviceManager));
+        }
+
+    }
+}
