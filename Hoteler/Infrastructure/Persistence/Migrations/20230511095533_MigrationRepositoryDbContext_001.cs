e187b9f9adcaf6935b2e2600ab031fd86aae2edc
Petr Kruchinin <kruchinin.pea@mmk.ru>
2023-05-11 14:59:12 +0500

    миграция 1

---
+++ Hoteler/Infrastructure/Persistence/Migrations/20230511095533_MigrationRepositoryDbContext_001.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationRepositoryDbContext_001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Town = table.Column<string>(type: "text", nullable: false),
+                    FullAddress = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Floor = table.Column<int>(type: "integer", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Number = table.Column<string>(type: "text", nullable: false),
+                    CurrentRoomStatus = table.Column<string>(type: "text", nullable: false),
+                    HotelId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Room_Hotel_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotelReviews",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Review = table.Column<string>(type: "text", nullable: false),
+                    Grade = table.Column<string>(type: "text", nullable: false),
+                    IsAnonymusReview = table.Column<bool>(type: "boolean", nullable: false),
+                    ReviewerId = table.Column<Guid>(type: "uuid", nullable: true),
+                    HotelId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelReviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HotelReviews_Hotel_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HotelReviews_User_ReviewerId",
+                        column: x => x.ReviewerId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookedByDatesRooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BookedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsBooked = table.Column<bool>(type: "boolean", nullable: false),
+                    BookingUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RoomId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookedByDatesRooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookedByDatesRooms_Room_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookedByDatesRooms_User_BookingUserId",
+                        column: x => x.BookingUserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookedByDatesRooms_BookingUserId",
+                table: "BookedByDatesRooms",
+                column: "BookingUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookedByDatesRooms_RoomId",
+                table: "BookedByDatesRooms",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelReviews_HotelId",
+                table: "HotelReviews",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelReviews_ReviewerId",
+                table: "HotelReviews",
+                column: "ReviewerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Room_HotelId",
+                table: "Room",
+                column: "HotelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookedByDatesRooms");
+
+            migrationBuilder.DropTable(
+                name: "HotelReviews");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Hotel");
+        }
+    }
+}
